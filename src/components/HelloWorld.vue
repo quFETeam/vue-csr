7db20dba9b4b2d47d783422a285cbc05e177d90d
luxin <398873800@qq.com>
2021-12-22 19:24:14 +0800

    test

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 
-defineProps<{ msg: string }>();
+const props = defineProps<{ msg: string }>();
 
 const count = ref(0);
+if (!props.msg) {
+  count.value = 1;
+}
 </script>
 
 <template>
